ef04b50fcea663ed822b63a3740babc38dec0024
mufrezadn <mufrezadn@gmail.com>
2019-11-09 23:53:57 -0800

    update

---
+++ labspy02.py
@@ -0,0 +1,11 @@
+a=int(input("bilangan1 = "))
+b=int(input("bilangan2 = "))
+c=int(input("bilangan3 = "))
+
+if a>b and a>c:
+        print (a, 'terbesar')
+elif b>a and b>c:
+        print (b, 'terbesar')
+else:
+        print (c, 'terbesar')
+        
